aaa413a23462d56942f4e143c42a951d53a74d0a
Alessandro Angelino <alessandro.angelino@arm.com>
2014-12-05 17:25:40 +0000

    New executable function: now using a secured-module print function

--- source/debug.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DEBUG_H__
-#define __DEBUG_H__
-
-#define MAX_STRING_SIZE 160
-
-extern void dputc(char data);
-extern int dprintf(const char* format, ...);
-
-#endif/*__DEBUG_H__*/
--- source/main.cpp
+++ source/main.cpp
@@ -1,27 +1,10 @@
-#include "mbed/mbed.h"
-#include "uvisor-lib/uvisor-lib.h"
-#include "uvisor-sectest/uvisor-sectest.h"
-#include "debug.h"
-
-/* LED is instantiated locally to isolate peripheral
- * instantiations from libc */
-void led_blinker(void)
-{
-    DigitalOut led(LED1);
-
-    led = 1;
-    wait(1.0);
-    led = 0;
-    wait(1.0);
-}
+#include <mbed/mbed.h>
+#include <uvisor-secprint/uvisor-secprint.h>
 
 int main(void) 
 {
-    dprintf("Hello World!\n");
-
     while(1) {
-        led_blinker();
-        dprintf("New result: 0x%08X\n", 42);
+        secprint();
     }
 
     return 0;
