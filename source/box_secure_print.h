c99b0b3c1683fcd637b984f1cc3b9063e3f0d1e2
Alessandro Angelino <alessandro.angelino@arm.com>
2015-03-11 13:32:06 +0000

    Updated file types

--- source/box_secure_print.h
+++ source/box_secure_print.h
@@ -13,8 +13,6 @@
 #ifndef __BOX_SECURE_PRINT_H__
 #define __BOX_SECURE_PRINT_H__
 
-#define BOX_SECURE_PRINT_ID CFG_BOX_SECURE_PRINT_ID
-
 void secure_print(void);
 
 #endif/*__BOX_SECURE_PRINT_H__*/
