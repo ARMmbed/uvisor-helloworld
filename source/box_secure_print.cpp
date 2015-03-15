8c2d241550c6b9edd0320e58cf06643c66f3cf3d
Alessandro Angelino <alessandro.angelino@arm.com>
2015-03-15 17:24:56 +0100

    Added actual call to secure gateway

--- source/box_secure_print.cpp
+++ source/box_secure_print.cpp
@@ -34,7 +34,7 @@ static const UvisorBoxAclItem g_box_acl[] = {
 	{PORTB,   sizeof(*PORTB), UVISOR_TACL_PERIPHERAL},
 };
 /* configure secure box compartnent */
-UVISOR_BOX_CONFIG(secure_print, g_box_acl, UVISOR_BOX_STACK_SIZE);
+UVISOR_BOX_CONFIG(secure_print_box, g_box_acl, UVISOR_BOX_STACK_SIZE);
 
 /* simple print function for the serial interface */
 void serial_printf(const char *c)
@@ -49,7 +49,7 @@ void serial_printf(const char *c)
 /* print a secret message
  *   if called directly, no security breach as it will run with the
  *   privileges of the caller*/
-void __secure_print(void)
+extern "C" void __secure_print(void)
 {
 	/* initialize serial object on first use */
 	if(!g_data.serial) {
@@ -70,5 +70,5 @@ void secure_print(void)
 	/* security transition happens here
 	 *   ensures that __secure_print() will run with the privileges
 	 *   of the secure_print box */
-	__secure_print();
+    secure_gateway(secure_print_box, __secure_print, 0, 0, 0, 0);
 }
