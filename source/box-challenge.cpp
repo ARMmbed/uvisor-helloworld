ARMmbed/uvisor-helloworld

commit 79011cbd174e0d749ce841fdfe9224605737caf7

Example of (failing) security breach attempt
a loop continuosly tries to compare its challenge with the protected
box-challenge password;

the on-board push-button, meanwhile, can be used to trigger an ISR that tries to
read the content of the protected box-challenge context;

if the access is allowed, the next time the loop content is executed, the
challenge will match the protected box-challenge password

source/box-challenge.cpp
@@ -16,8 +16,8 @@
 #include "box-challenge-hw.h"
 
 typedef struct {
-    bool initialized;
     uint8_t secret[CHALLENGE_SIZE];
+    bool initialized;
 } BoxContext;
 
 /* create ACLs for secret data section */
@@ -26,10 +26,17 @@ BOX_CHALLENGE_ACL(g_box_acl);
 /* configure secure box compartnent and reserve box context */
 UVISOR_BOX_CONFIG(box_challenge, g_box_acl, UVISOR_BOX_STACK_SIZE, BoxContext);
 
-static void __randomize_new_secret(void)
+void *g_box_context;
+
+static void randomize_new_secret(void)
 {
     /* FIXME replace with HW-RNG */
-    memset(uvisor_ctx->secret, 0, sizeof(uvisor_ctx->secret));
+    memset(uvisor_ctx->secret, 1, sizeof(uvisor_ctx->secret));
+
+    /* the address of the context is stored and exposed, only for testing
+     * purposes; uvisor protection ensures that accesses to this memory location
+     * outside the box secure context will fail */
+    g_box_context = (void *) uvisor_ctx;
 }
 
 static bool secure_compare(const uint8_t *src, const uint8_t *dst, int len)
@@ -60,7 +67,7 @@ UVISOR_EXTERN bool __verify_secret(const uint8_t *secret, int len)
     /* generate new secret on the first run
      * FIXME enable clocks for HW-RNG */
     if(!uvisor_ctx->initialized)
-        __randomize_new_secret();
+        randomize_new_secret();
 
     return secure_compare(secret, uvisor_ctx->secret, len);
 }

source/box-challenge.h
@@ -15,6 +15,7 @@
 
 #define CHALLENGE_SIZE 16
 
-UVISOR_EXTERN bool verify_secret(const uint8_t *secret, int len);
+extern bool verify_secret(const uint8_t *secret, int len);
+extern void *g_box_context;
 
 #endif/*__BOX_CHALLENGE_H__*/

source/btn.cpp
@@ -20,10 +20,10 @@ InterruptIn btn(MAIN_BTN);
 
 static void btn_onpress(void)
 {
-    uint8_t challenge[CHALLENGE_SIZE];
-
-    /* attempt copy from box_challenge context */
-    /* TODO */
+    /* attempt copy from box_challenge context
+     * we know the context is properly aligned so we try to obtain a carbon copy
+     * of its memory location */
+    memcpy(&g_challenge, g_box_context, sizeof(g_challenge));
 }
 
 void btn_init(void)

source/main-hw.h
@@ -13,6 +13,11 @@
 #ifndef __MAIN_HW_H__
 #define __MAIN_HW_H__
 
+/* the vector containing the challenge is shared with the push-button ISR, so
+ * that it can attempt to access it from an IRQ context */
+#include "box-challenge.h"
+extern uint8_t g_challenge[CHALLENGE_SIZE];
+
 /* target specifc ACLs */
 #if   defined(TARGET_LIKE_FRDM_K64F_GCC)
 

source/main.cpp
@@ -24,26 +24,30 @@ UVISOR_SET_MODE_ACL(UVISOR_ENABLED, g_main_acl);
 
 DigitalOut led(MAIN_LED);
 
+uint8_t g_challenge[CHALLENGE_SIZE];
+
 void app_start(int, char *[])
 {
-    uint8_t challenge[CHALLENGE_SIZE];
-
     /* reset challenge */
-    memset(&challenge, 0, sizeof(challenge));
+    memset(&g_challenge, 0, sizeof(g_challenge));
 
     /* turn LED off */
-    led = false;
+    led = LED_OFF;
 
     /* configure pushbutton */
     btn_init();
 
     while(1) {
-
-        /* check for secret, blink if it's wrong */
-        if(verify_secret(challenge, sizeof(challenge)))
-            led = true;
+        /* check for secret */
+        if(verify_secret(g_challenge, sizeof(g_challenge)))
+        {
+            /* send LED pulse if the secret was found */
+            led = LED_ON;
+            wait(.4);
+            led = LED_OFF;
+        }
 
         /* wait before trying again */
-        wait(1.0);
+        wait(2.0);
     }
 }

commit a574eb816af0c8efefc95f98c868b16bceb87e9e

Fix initialization bug in secure box
The secret was repeatedly initialized due to an unchanged flag. The bug
was originally not observed since the secret is currently not
randomized, so it stayed always the same across subsequent reading
attempts.

Fixes: 79011cb ("Example of (failing) security breach attempt")

source/box-challenge.cpp
@@ -77,10 +77,12 @@ UVISOR_EXTERN bool __verify_secret(const uint8_t *secret, int len)
 
     /* FIXME verify that secret pointer points outside of box stack context */
 
-    /* generate new secret on the first run
-     * FIXME enable clocks for HW-RNG */
-    if(!uvisor_ctx->initialized)
+    /* Generate new secret on the first run. */
+    /* FIXME enable clocks for HW-RNG */
+    if (!uvisor_ctx->initialized) {
         randomize_new_secret();
+        uvisor_ctx->initialized = true;
+    }
 
     return secure_compare(secret, uvisor_ctx->secret, len);
 }